46e9cc375152ce1cf773de3cf69400acee4933fd
Leonardo Ledda <leonardoledda@gmail.com>
2024-02-05 16:07:05 +0100

    RandomNumberGenerator: More fixes/updates
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- app/src/main/java/com/leddaz/rng/MainActivity.kt
+++ app/src/main/java/com/leddaz/rng/MainActivity.kt
@@ -3,12 +3,9 @@ package com.leddaz.rng
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
-import kotlin.random.nextInt
-
-const val EXTRA_MESSAGE = "com.leddaz.rng.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    /** Called when the user taps the Confirm button */
+    /**
+     * Called when the user taps the Confirm button.
+     */
+    @Suppress("UNUSED_PARAMETER")
     fun genNum(view: View) {
         val minNumber = findViewById<EditText>(R.id.MinTextBox)
         val min = minNumber.text.toString()
         val maxNumber = findViewById<EditText>(R.id.MaxTextBox)
         val max = maxNumber.text.toString()
-        val rnd =  Random.nextInt(min.toInt(), max.toInt())
-        RandomNumber.text = rnd.toString()
-        RandomNumber.visibility = View.VISIBLE
+        val rnd = Random.nextInt(min.toInt(), max.toInt())
+        val result = findViewById<TextView>(R.id.RandomNumber)
+        result.text = rnd.toString()
+        result.visibility = View.VISIBLE
     }
 }
